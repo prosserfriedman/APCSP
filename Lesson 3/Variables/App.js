4f290e8a8ef847b05898ef987626262ab3950912
Prosser Friedman <prosserfriedman@ORRHS.ORG>
2020-02-12 10:11:31 -0500

    add event listeners

---
+++ Lesson 3/Variables/App.js
@@ -0,0 +1,17 @@
+// const add = 7 + 3;
+// const sub = 7 - 3;
+// const mult = 7 * 3;
+// const div = 7 / 3;
+// const paren = (83 + 92 + 86 + 94) / 4;
+// console.log(add,sub,mult,div,paren)
+
+// // let score = 0
+// // console.log('Current Score:'+ score)
+
+// let numLikes = 7
+// console.log('So far ' + numLikes + ' people like this page')
+
+// let score = 0;
+// let lives = 3;
+// console.log("'You have' + lives + 'lives left with score' + score");
+
